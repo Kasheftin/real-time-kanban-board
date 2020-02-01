96e5be7b8d4953500b6261cf0dbecb8927cdc857
Kasheftin <Kasheftin@gmail.com>
2020-02-01 16:03:49 +0200

    socket restricted to /ws/ path

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ CREATE TABLE `tasks` (
 
 const app = require('express')()
 const server = require('http').Server(app)
-const io = require('socket.io')(server)
+const io = require('socket.io')(server, {path: '/ws/'})
 const bodyParser = require('body-parser')
 const mysql = require('mysql2')
 
